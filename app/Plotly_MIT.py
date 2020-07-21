7610eadce536270b38363bc41df476f8c297b267
Ryan Donlan <donlan.ryan@gmail.com>
2020-07-21 13:08:01 -0400

    fixing wsgi.py

--- app/Plotly_MIT.py
+++ app/Plotly_MIT.py
@@ -13,6 +13,15 @@ styles = {
         'overflowX': 'scroll'
     }
 }
+
+from flask import Flask 
+  
+app = Flask(__name__) 
+  
+@app.route("/") 
+def home_view(): 
+        return "<h1>Welcome to Geeks for Geeks</h1>"
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server # the Flask app
 
