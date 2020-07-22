81688bd359e30ddd145650b7a746ca2872836618
Ryan Donlan <donlan.ryan@gmail.com>
2020-07-22 17:22:08 -0400

    updating ML

--- machine_learning.py
+++ machine_learning.py
@@ -12,20 +12,23 @@ keep_default_na=True
 matches_df = all_matches_df[0:59].fillna(0)
 #print(matches_df)
 
-# for each row starting at index 2 we need to look at each column index 4-35 and find keyword 'match'
-# iterate through every row
 mentor_matches_dict = {}
+
+# iterate through each row
 for i in range(1,59):
     row = matches_df.iloc[i][4:36]
     mentor = matches_df.iloc[i][0]
     solver_matches = []
+    # iterate through all cells within the row
     for j in range(0,32):
         potential_match = str(row.iloc[j])
+        # check if a match
         if potential_match == 'Match' or potential_match == 'Match?':
             solver = matches_df.columns[j+4]
             solver_matches.append(solver)
     mentor_matches_dict[mentor] = solver_matches
 
+# prints out dict to check
 for mentor in mentor_matches_dict:
     print("Mentor: " + str(mentor))
     print(len(mentor_matches_dict[mentor]))
