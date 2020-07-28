d4d8f395092ddb943f2687df409f54453f0f3d07
Ryan Donlan <donlan.ryan@gmail.com>
2020-07-28 19:48:30 -0400

    adding required imports

--- app/main.py
+++ app/main.py
@@ -7,7 +7,10 @@ import io
 import dash_auth
 
 # will turn excel into csv
-from split_xlsx import ExceltoCSV
+# from split_xlsx import ExceltoCSV
+import csv
+import xlrd
+import sys
 
 # for app
 import os
@@ -110,6 +113,39 @@ def generate_table(dataframe, max_rows=10):
     ])
 
 
+# This allows the input excel file to be turned into csv files which will be used
+# to calculate the information required for pairing
+def ExceltoCSV(excel_file, csv_file_base_path, csv_folder = "uploaded_excel_to_csv/"):
+    """
+    inputs:
+    Excel file, str - name of the original file
+    csv_file_base_path, str - folder location where each file will be stored
+    excel_folder, str - a folder with this name will be created in csv_base_file_path with sheet names
+                        with excel file. If excel file has sheets then each sheet will be stored as
+                        a seperte csv file
+
+    """    
+
+    if not os.path.isdir(csv_file_base_path+csv_folder): 
+        os.mkdir(csv_file_base_path+csv_folder)
+    full_path = csv_file_base_path+csv_folder
+    workbook = xlrd.open_workbook(excel_file)
+
+    for sheet_name in workbook.sheet_names():
+        print('processing - ' + sheet_name)
+        worksheet = workbook.sheet_by_name(sheet_name)
+        csv_file_full_path = full_path + sheet_name.lower().replace(" - ", "_").replace(" ","_") + '.csv'
+        csvfile = open(csv_file_full_path, 'w',encoding='utf-8')
+        writetocsv = csv.writer(csvfile, quoting = csv.QUOTE_ALL, )
+        for rownum in range(worksheet.nrows):
+            writetocsv.writerow(
+    #                 list(x.encode('utf-8') if type(x) == type(u'') else x for x in worksheet.row_values(rownum)
+                    worksheet.row_values(rownum)
+                )
+            
+        csvfile.close()
+        print( "{} has been saved at {}".format(sheet_name, csv_file_full_path))
+
 # Method used to parse files from upload button
 def parse_contents(contents, filename, date):
     '''
@@ -151,6 +187,8 @@ def parse_contents(contents, filename, date):
     #     ),
     # ])
     return None
+
+
 
 # APP LAYOUT
 app.layout = html.Div(children=[
